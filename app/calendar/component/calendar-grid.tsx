b9404da7c8c550c6956c95f39634dcce1ae708c4
Giovanne Colli <cgiovanne45@gmail.com>
2025-09-27 14:28:38 -0300

    finish calendar-grid

--- app/calendar/component/calendar-grid.tsx
+++ app/calendar/component/calendar-grid.tsx
@@ -1,13 +1,19 @@
 "use client"
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import {toast, Toaster} from 'sonner';
 
 const days = Array.from({ length: 30 }, (_, index) => index + 1);
 const hours = Array.from({ length: 24 }, (_, index) => index);
 
+const monthArr = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
 export default function CalendarGrid() {
-    const [month, setMonth] = useState('Janeiro');
+    const [nowDate, setNowDate] = useState(new Date());
+    const [month, setMonth] = useState(9);
     const [year, setYear] = useState(2025);
+    const intervalRef = useRef(null);
+    const elemOverflow = useRef(null);
     const [events, setEvents] = useState({
         1: [
             {
@@ -16,7 +22,7 @@ export default function CalendarGrid() {
                 eventName: 'Evento Teste',
                 border: '#d81bb8ff',
                 color: '#d81bb856',
-                id: '1234' 
+                id: '1234'
             }
         ],
         4: [
@@ -24,9 +30,9 @@ export default function CalendarGrid() {
                 startSeconds: 1800,
                 endSeconds: 10800,
                 eventName: 'Evento Teste',
-                border: '#d81bb8ff',
-                color: '#d81bb856',
-                id: '1235' 
+                border: '#1bd2d8ff',
+                color: '#1ba2d856',
+                id: '1235'
             },
             {
                 startSeconds: 11800,
@@ -34,10 +40,34 @@ export default function CalendarGrid() {
                 eventName: 'Evento Teste',
                 border: '#d81bb8ff',
                 color: '#d81bb856',
-                id: '1236' 
+                id: '1236'
+            }
+        ],
+        27: [
+            {
+                startSeconds: 43000,
+                endSeconds: 52000,
+                eventName: 'Evento Teste',
+                border: '#d81bb8ff',
+                color: '#d81bb856',
+                id: '12396'
             }
         ]
     })
+
+    useEffect(() => {
+        intervalRef.current = setInterval(() => {
+            setNowDate(new Date());
+        }, 60000);
+        return () => clearInterval(intervalRef.current);
+    }, [])
+
+    useEffect(() => {
+        const tableContainer = elemOverflow.current.querySelector('[data-slot="table-container"]');
+        if(tableContainer && isActualDate()) {
+            tableContainer.scrollTo((nowDate.getDate() - 2) * 160, getTop() - 60*2)
+        }
+    }, [])
 
     function getHeigth(startSeconds: number, endSeconds: number) {
         const diff = endSeconds - startSeconds;
@@ -48,14 +78,41 @@ export default function CalendarGrid() {
         return `${(startSeconds / 3600) * 60}px`
     }
 
+    function isActualDate() {
+        return month === (nowDate.getMonth() + 1) && year === nowDate.getFullYear()
+    }
+
+    function canRenderLine(date: number) {
+        return isActualDate() && date === nowDate.getDate()
+    }
+
+    function getTop() {
+        return nowDate.getHours() * 60 + nowDate.getMinutes()
+    }
+
+    function topInPx() {
+        const top = getTop() - 2;
+        return `${top}px`
+    }
+
+    function toaster() {
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },}
+        )
+    }
+
     return (
         <div className="flex flex-col">
-            <div>
-                {month} - {year}
-            </div>
-            <div className="overflow-visible">
-                <Table className="w-full">
-                    <TableHeader>
+            <div ref={elemOverflow}  className="flex flex-col h-[90vh] overflow-auto">
+                <div className="p-3" onClick={() => toaster()}>
+                    {monthArr[month - 1]} - {year}
+                </div>
+                <Table className="w-full relative">
+                    <TableHeader className="sticky top-0 bg-white z-10">
                         <TableRow>
                             {days.map(day => <TableHead className="w-40" key={day}>{day}</TableHead>)}
                         </TableRow>
@@ -65,16 +122,17 @@ export default function CalendarGrid() {
                             {days.map(day => <TableCell className="w-full p-0" key={day}>
                                 <div className="divide-y divide-gray-300 w-40 relative">
                                     {hours.map(hour => <div className="relative" style={{ height: '60px' }} key={`${day}_${hour}`}>
-                                        <div className="absolute top-1 left-3">{hour}</div>
+                                        <div className="absolute top-1 font-extralight left-3">{String(hour).padStart(2, '0')}:00</div>
                                     </div>)}
                                     {
-                                        (events[day] ?? []).map(event => <div key={event.id} className="absolute w-full min-h-4 px-2" style={{ top: calcTop(event.startSeconds) }}>
+                                        (events[day] ?? []).map(event => <div key={event.id} className="absolute w-full min-h-4 px-2 cursor-pointer" style={{ top: calcTop(event.startSeconds) }}>
                                             <div className="w-full rounded-md p-2" style={{ background: event.color, height: getHeigth(event.startSeconds, event.endSeconds), border: `3px solid ${event.border}` }}>
                                                 {event.eventName}
                                             </div>
                                         </div>
-                                    )
+                                        )
                                     }
+                                    {canRenderLine(day) && <div className="w-full absolute h-1 bg-red-400" style={{ top: topInPx() }}></div>}
                                 </div>
                             </TableCell>)
                             }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import SystemHeader from "@/components/ui/SystemHeader";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Toaster position="top-center"/>
         <SystemHeader/>
         {children}
       </body>
